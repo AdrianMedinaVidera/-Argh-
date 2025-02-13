ccb2867f38189be19e2c02367fdd0cd140525c47
Adrián Medina Videra <adrianmedinavidera@gmail.com>
2025-02-13 13:10:07 +0100

    Update Argh.java

--- Argh/src/Argh.java
+++ Argh/src/Argh.java
@@ -1,1 +1,42 @@
+package AppArgh;
 
+import Personaje.Personaje;
+import Producto.Producto;
+
+public class Argh {
+	public static void main(String[] args) {
+		Producto[] producto = new Producto[7];
+		//Producto objetoEquipado = new Producto("Pocion", 20, 2);
+		// Creación de todos los objetos del juego.
+		Producto minipocion = new Producto("Minipoción", 20, 0);
+		Producto pocion = new Producto("Poción", 30, 0);
+		Producto superpocion = new Producto("Superpoción", 60, 0);
+		
+		Producto baculo = new Producto("Báculo", 15, 0);
+		Producto espada = new Producto("Espada", 30, 0);
+		Producto mandoble = new Producto("Mandoble", 120, 0);
+		
+		Producto varitaMadera = new Producto("Varita Madera", 15, 0);
+		Producto varitaPlastico = new Producto("Varita Plástico", 30, 0);
+		Producto varitaOro = new Producto("Varita Oro", 120, 0);
+		
+		Producto escudito = new Producto("Escudito", 15, 0);
+		Producto escudo = new Producto("Escudo", 30, 0);
+		Producto escudazo = new Producto("Escudazo", 120, 0);
+		
+		Producto capita = new Producto("Capita", 15, 0);
+		Producto capa = new Producto("Capa", 30, 0);
+		Producto capaza = new Producto("Capaza", 120, 0);
+		
+		Producto alas = new Producto("Alas", 30, 0);
+		Producto pedo = new Producto("Pedo", 30, 0);
+		Producto objetoMisterioso = new Producto("???", 50, 0);
+		Producto redbull = new Producto("Redbull", 10, 0);
+		Producto caramelo = new Producto("Caramelo", 70, 0);
+		
+		
+		
+		
+		
+	}
+}
