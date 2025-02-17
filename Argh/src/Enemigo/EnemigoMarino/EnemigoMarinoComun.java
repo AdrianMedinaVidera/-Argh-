7051d66bf8da1608a1497ca1961103e053088991
Carlolopespesia <carlolopespesia@gmail.com>
2025-02-17 20:54:43 +0100

    Enemigos marinos comunes completados

--- Argh/src/Enemigo/EnemigoMarino/EnemigoMarinoComun.java
+++ Argh/src/Enemigo/EnemigoMarino/EnemigoMarinoComun.java
@@ -45,7 +45,7 @@ public class EnemigoMarinoComun extends EnemigoMarino {
             velocidad -= 20;
             dañoMagico += 150;
         }else if(enemigoSeleccionado.equals("Trilobite")){
-            System.out.println("El enemigo ha usado el ataque Succión de concha");
+            System.out.println("El enemigo ha usado el ataque Succión de caparazón");
             //El trilobite crea vacío y quita 30 puntos de vida, 10 de daño mágico al personaje y aumenta su resistencia fisica en 20 y su velocidad en nado.
             personaje.setVida(personaje.getVida() - 30);
             personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 10));
@@ -65,22 +65,25 @@ public class EnemigoMarinoComun extends EnemigoMarino {
     public void ataque3(Personaje personaje) {
         if (enemigoSeleccionado.equals("Amonite")) {
             System.out.println("El enemigo ha usado el ataque Furia Egipcia");
-            // El amonite 
-            //Esto es nuevo, a ver si se imprime en el repo
+            // El amonite piensa en el origen de su nombre y se enfurece, causando daño fisico y mágico en 20 y su velocidad en nado donde se resta 10 puntos de resistencia mágica.
+            personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 20));
+            personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 20));
+            resistenciaMagica -= 10;
+            velocidad *= nado/100;
         }else if(enemigoSeleccionado.equals("Trilobite")){
-            System.out.println("El enemigo ha usado el ataque Succión de concha");
-            //El trilobite crea vacío y quita 30 puntos de vida, 10 de daño mágico al personaje y aumenta su resistencia fisica en 20 y su velocidad en nado.
-            personaje.setVida(personaje.getVida() - 30);
-            personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 10));
-            resistenciaFisica += 20;
-            velocidad *= nado/100;
+            System.out.println("El enemigo ha usado el ataque Revuelto de tierras");
+            //El trilobite hace una danza donde mueve arena y causa 40 de daño mágico y 5 de daño físcio, disminuyendo su resistencia mágica y física en 15 puntos
+            personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 40));
+            personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 5));
+            resistenciaMagica -= 15;
+            resistenciaFisica -= 15; 
         }else if(enemigoSeleccionado.equals("Zooplacton")){
-            System.out.println("El enemigo ha usado el ataque Mordisquito minúsculo");
-            //El zooplacton quita 5 puntos de daño físico y aumenta su resistencia magica en 10 y su resistencia física en 20 y su velocidad en nado.
-            personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 10));
-            resistenciaMagica += 10;
-            resistenciaFisica += 20;
-            velocidad *= nado/100;
+            System.out.println("El enemigo ha usado el ataque Marea microscópica");
+            //El zooplacton genera una corriente invisible en el agua que atrae microdepredadores de su ecosistema, creando un enjambre de microorganismos que atacan al enemigo,
+            //causando 60 de daño físico, reduciendo la resistencia física del enemigo en 10 y aumentando su velocidad en nado con un plus.
+            personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 60));
+            personaje.setResistenciaFisica(personaje.getResistenciaFisica() - 10);
+            velocidad *= (nado+30)/100;
         }
     }
 
