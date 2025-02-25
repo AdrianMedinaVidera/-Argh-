9b951842fb4d152cbec24afc9c20bf1b029ad040
AdrianMedinaVidera <adrianmedinavidera@gmail.com>
2025-02-25 16:43:17 +0100

    enemigosMarinosMenus;

--- AppArgh/src/Enemigo/EnemigoMarino/EnemigoMarinoComun.java
+++ AppArgh/src/Enemigo/EnemigoMarino/EnemigoMarinoComun.java
@@ -22,31 +22,43 @@ public class EnemigoMarinoComun extends EnemigoMarino {
     public void ataque1(Personaje personaje) {
         switch(enemigoSeleccionado) {
             case "Amonite":
-                System.out.println("El enemigo ha usado el ataque Mordisco del devónico");
-                // El amonite muerde al personaje, haciendo 60 de daño fisico según del atributo nado y aumenta su daño fisico en 10.
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha usado el ataque              ║");
+                System.out.println("║  Mordisco del devónico                      ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + (60 * nado / 100)));
                 dañoFisico += 10;
-                System.out.println("El enemigo ha aumentado su daño físico");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha aumentado su daño físico     ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
             
             case "Trilobite":
-                System.out.println("El enemigo ha usado el ataque Restriego de caparazón cámbrico");
-                //El trilobite se restriega contra el personaje, haciendo 40 de daño fisico y aumenta su resistencia fisica en 10 y su velocidad en nado.
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha usado el ataque              ║");
+                System.out.println("║  Restriego de caparazón cámbrico            ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 40));
                 resistenciaFisica += 10;
                 velocidad *= nado/100;
-                System.out.println("El enemigo ha aumentado su resistencia física ");
-                System.out.println("El enemigo ha aumentado su velocidad ");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha aumentado su resistencia     ║");
+                System.out.println("║  física y su velocidad                      ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
             
             case "Zooplacton":
-                System.out.println("El enemigo ha usado el ataque Involuntario precámbrico");
-                //El zooplacton se lanza contra el personaje, haciendo 50 de daño fisico y aumenta su resistencia magica en 10 y su velocidad en nado.
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha usado el ataque              ║");
+                System.out.println("║  Involuntario precámbrico                   ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 50));
                 resistenciaMagica += 10;
                 velocidad *= nado/100;
-                System.out.println("El enemigo ha aumentado su resistencia mágica ");
-                System.out.println("El enemigo ha aumentado su velocidad ");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha aumentado su resistencia     ║");
+                System.out.println("║  mágica y su velocidad                      ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
         }
     }
@@ -55,17 +67,24 @@ public class EnemigoMarinoComun extends EnemigoMarino {
     public void ataque2(Personaje personaje) {
         switch(enemigoSeleccionado) {
             case "Amonite":
-                System.out.println("El enemigo ha usado el ataque Cuerno tormentoso");
-                // El amonite aumenta sus revoluciones generando un campo electroestatico que hace 5 puntos de daño mágico, disminuye su velocidad en 20% y aumenta su dañomágico en 150
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha usado el ataque              ║");
+                System.out.println("║  Cuerno tormentoso                          ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 5));
                 velocidad *= 0.8;
                 dañoMagico += 150;
-                System.out.println("El enemigo ha disminuido su velocidad ");
-                System.out.println("El enemigo ha aumentado su daño mágico ");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha disminuido su velocidad      ║");
+                System.out.println("║  y aumentado su daño mágico                 ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
             
             case "Trilobite":
-                System.out.println("El enemigo ha usado el ataque Succión de caparazón");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha usado el ataque              ║");
+                System.out.println("║  Succión de caparazón                       ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 //El trilobite crea vacío y quita 30 puntos de vida, 10 de daño mágico al personaje y aumenta su resistencia fisica en 20 y su velocidad en nado.
                 personaje.setVida(personaje.getVida() - 30);
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 10));
@@ -73,18 +92,26 @@ public class EnemigoMarinoComun extends EnemigoMarino {
                 velocidad *= nado/100;
                 System.out.println("El enemigo ha aumentado su resistencia física");
                 System.out.println("El enemigo ha aumentado su velocidad");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha aumentado su resistencia     ║");
+                System.out.println("║  física y ha  aumentado su velocidad        ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
             
             case "Zooplacton":
-                System.out.println("El enemigo ha usado el ataque Mordisquito minúsculo");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El enemigo ha usado el ataque              ║");
+            System.out.println("║  Mordisquito minúsculo                      ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 //El zooplacton quita 5 puntos de daño físico y aumenta su resistencia magica en 10 y su resistencia física en 20 y su velocidad en nado.
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 10));
                 resistenciaMagica += 10;
                 resistenciaFisica += 20;
                 velocidad *= nado/100;
-                System.out.println("El enemigo ha aumentado su resistencia mágica");
-                System.out.println("El enemigo ha aumentado su resistencia física");
-                System.out.println("El enemigo ha aumentado su velocidad");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha aumentado sus resistencias   ║");
+                System.out.println("║  y su velocidad                             ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
         }
     }
@@ -93,35 +120,49 @@ public class EnemigoMarinoComun extends EnemigoMarino {
     public void ataque3(Personaje personaje) {
         switch(enemigoSeleccionado) {
             case "Amonite":
-                System.out.println("El enemigo ha usado el ataque Furia Egipcia");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El enemigo ha usado el ataque              ║");
+            System.out.println("║  Furia egipcia                              ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // El amonite piensa en el origen de su nombre y se enfurece, causando daño fisico y mágico en 20 y su velocidad en nado donde se resta 10% de puntos de resistencia mágica.
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 20));
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 20));
                 resistenciaMagica *= 0.9;
                 velocidad *= nado/100;
-                System.out.println("El enemigo ha disminuido su resistencia mágica ");
-                System.out.println("El enemigo ha aumentado su velocidad ");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha disminuido su resistencia    ║");
+                System.out.println("║  magica y ha aumentado su velocidad         ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
             
             case "Trilobite":
-                System.out.println("El enemigo ha usado el ataque Revuelto de tierras");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El enemigo ha usado el ataque              ║");
+            System.out.println("║  Revuelta de tierras                        ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 //El trilobite hace una danza donde mueve arena y causa 40 de daño mágico y 5 de daño físcio, disminuyendo su resistencia mágica y física en 15 puntos
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 40));
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 5));
                 resistenciaMagica *= 0.85;
                 resistenciaFisica *= 0.85;
-                System.out.println("El enemigo ha disminuido su resistencia mágica ");
-                System.out.println("El enemigo ha disminuido su resistencia física ");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha disminuido sus resistencias  ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
             
             case "Zooplacton":
-                System.out.println("El enemigo ha usado el ataque Marea microscópica");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El enemigo ha usado el ataque              ║");
+            System.out.println("║  Marea Microscopica                         ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 //El zooplacton genera una corriente invisible en el agua que atrae microdepredadores de su ecosistema, creando un enjambre de microorganismos que atacan al enemigo,
                 //causando 60 de daño físico, reduciendo la resistencia física del enemigo en 10 y aumentando su velocidad en nado con un plus.
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 60));
                 personaje.setResistenciaFisica(personaje.getResistenciaFisica() - 10);
                 velocidad *= (nado+30)/100;
-                System.out.println("El enemigo ha aumentado su velocidad ");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El enemigo ha aumentado su velocidad       ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
         }
     }
--- AppArgh/src/Enemigo/EnemigoMarino/EnemigoMarinoJefe.java
+++ AppArgh/src/Enemigo/EnemigoMarino/EnemigoMarinoJefe.java
@@ -22,32 +22,46 @@ public class EnemigoMarinoJefe extends EnemigoMarino {
     public void ataque1(Personaje personaje) {
         switch(jefeSeleccionado) {
             case "Sirena":
-                System.out.println("El jefe enemigo ha usado el ataque Canto de las Profundidades");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Canto de las profundidades                 ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // La sirena entona una melodía hipnótica que confunde al jugador, realizando 150 de daño mágico y proporcionando el estado somnoliento.
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 150));
                 personaje.setEstaSomnoliento(true);
-                System.out.println("El personaje está somnoliento...");
+                System.out.println("╔═════════════════════════════════════════════╗");
+                System.out.println("║  El personaje esta somnoliento...           ║");
+                System.out.println("╚═════════════════════════════════════════════╝");
                 break;
             
             case "Patricio":
-                System.out.println("El jefe enemigo ha usado el ataque Abrazo Mortal");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Abrazo Mortal                              ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // Patricio agarra al personaje con sus brazos regordetes, si éste no es lo suficientemente fuerte, lo aplasta. Realiza 100 de daño físico y si el jugador tiene menos del 20% de la vida máxima, es ejecutado.
                 int vidaInicial = (int) (100 * Math.pow(1.1, personaje.getNivel()));
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 100));
                 if (personaje.getVida() < vidaInicial/5){
-                    System.out.println("Has sido ejecutado.");
+                    System.out.println("╔══════════════════════════╗");
+                    System.out.println("║  Has sido ejecutado.     ║");
+                    System.out.println("╚══════════════════════════╝");
                     personaje.setVida(0);
                 }
                 break;
             
             case "Kraken":
-                System.out.println("El jefe enemigo ha usado el ataque Tentáculos Aplastantes");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Tentaculos aplastantes                     ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // El Kraken usa sus tentáculos para aplastar al jugador, realizando 120 de daño físico y reduciendo sus resistencias un 20%.
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico - personaje.getResistenciaFisica()) + 120));
                 personaje.setResistenciaFisica((int) (personaje.getResistenciaFisica() * 0.8));
                 personaje.setResistenciaMagica((int) (personaje.getResistenciaMagica() * 0.8));
-                System.out.println("El enemigo ha reducido su Resistencia Física");
-                System.out.println("El enemigo ha reducido su Resistencia Mágica");
+                System.out.println("╔════════════════════════════════════════════════╗");
+                System.out.println("║  El Jefe enemigo ha reducido sus resistencias  ║");
+                System.out.println("╚════════════════════════════════════════════════╝");
                 break;
         }
     }
@@ -56,7 +70,10 @@ public class EnemigoMarinoJefe extends EnemigoMarino {
     public void ataque2(Personaje personaje) {
         switch(jefeSeleccionado) {
             case "Sirena":
-                System.out.println("El jefe enemigo ha usado el ataque Bendición de Poseidón");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Bendicion de Poseidon                      ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // La sirena recibe la bendición de Poseidón, restaurando 100 de vida y aumenta su velocidad y daño en 20%, pero reduce sus resistencias un 20%
                 vida += 100;
                 dañoFisico *= 1.2;
@@ -64,26 +81,33 @@ public class EnemigoMarinoJefe extends EnemigoMarino {
                 velocidad *= 1.2;
                 resistenciaFisica = (int) (resistenciaFisica * 0.8);
                 resistenciaMagica = (int) (resistenciaMagica * 0.8);
-                System.out.println("El enemigo ha aumentado su Vida ");
-                System.out.println("El enemigo ha aumentado su Daño Físico ");
-                System.out.println("El enemigo ha aumentado su Daño Mágico ");
-                System.out.println("El enemigo ha aumentado su Velocidad ");
-                System.out.println("El enemigo ha reducido su Resistencia Física");
-                System.out.println("El enemigo ha reducido su Resistencia Mágica");
+                System.out.println("╔══════════════════════════════════════════════════════╗");
+                System.out.println("║  El Jefe enemigo ha aumentado TODAS sus estadisticas ║");
+                System.out.println("╚══════════════════════════════════════════════════════╝");
                 break;
             
             case "Patricio":
-                System.out.println("El jefe enemigo ha usado el ataque Furia Estrellada");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Furia Estrellada                           ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // Patricio entra en un estado de furia estrellada, aumentando enormemente su ataque. Aumenta por 2 su daño físico.
                 dañoFisico *= 2;
-                System.out.println("El enemigo ha aumentado su Daño Físico");
+                System.out.println("╔══════════════════════════════════════════════════╗");
+                System.out.println("║  El Jefe enemigo ha aumentado su daño fisico     ║");
+                System.out.println("╚══════════════════════════════════════════════════╝");
                 break;
             
             case "Kraken":
-                System.out.println("El jefe enemigo ha usado el ataque Corrientes Abisales");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Corrientes Abisales                        ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // El Kraken controla las corrientes marinas, aumentando exageradamente su velocidad un 777%
                 velocidad *= 8.77;
-                System.out.println("El enemigo ha aumentado su Velocidad");
+                System.out.println("╔══════════════════════════════════════════════════╗");
+                System.out.println("║  El Jefe enemigo ha aumentado su velocidad       ║");
+                System.out.println("╚══════════════════════════════════════════════════╝");
                 break;
         }
     }
@@ -92,25 +116,39 @@ public class EnemigoMarinoJefe extends EnemigoMarino {
     public void ataque3(Personaje personaje) {
         switch(jefeSeleccionado) {
             case "Sirena":
-                System.out.println("El jefe enemigo ha usado el ataque Tridente Maldito");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Tridente Maldito                           ║");
+            System.out.println("╚═════════════════════════════════════════════╝");;
                 // La sirena invoca un tridente maldito que ignora las defensas físicas del rival, realizando 120 de daño mágico
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoFisico + 120)));
                 break;
             
             case "Patricio":
-                System.out.println("El jefe enemigo ha usado el ataque Vórtice Estelar");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Vortice Estelar                            ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // Patricio crea un vórtice que absorbe la energía vital del rival, realizando 120 de daño mágico y absorbiendo vida
                 int dañoRealizado = (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 120);
                 personaje.setVida(personaje.getVida() - dañoRealizado);
                 vida += dañoRealizado/2.5;
-                System.out.println("El enemigo ha aumentado su Vida");
+                System.out.println("╔═══════════════════════════════════════════╗");
+                System.out.println("║  El Jefe enemigo ha aumentado su vida     ║");
+                System.out.println("╚═══════════════════════════════════════════╝");
                 break;
             
             case "Kraken":
-                System.out.println("El jefe enemigo ha usado el ataque Tinta de las Profundidades");
+            System.out.println("╔═════════════════════════════════════════════╗");
+            System.out.println("║  El jefe enemigo ha usado el ataque         ║");
+            System.out.println("║  Tinta de las profundiades                  ║");
+            System.out.println("╚═════════════════════════════════════════════╝");
                 // El Kraken libera una nube de tinta mágica que aturde al jugador, realizando 120 de daño mágico y dejándolo somnoliento
                 personaje.setVida(personaje.getVida() - (int) (0.5 * (dañoMagico - personaje.getResistenciaMagica()) + 120));
                 personaje.setEstaSomnoliento(true);
+                System.out.println("╔═════════════════════════════════════╗");
+                System.out.println("║  El personaje esta somnoliento...   ║");
+                System.out.println("╚═════════════════════════════════════╝");
                 break;
         }
     }
